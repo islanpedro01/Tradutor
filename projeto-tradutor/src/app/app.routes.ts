b31c8410be23c8bf10c87214403f0c83d4916e3c
islanpedro01 <islanpedro.12@hotmail.com>
2024-11-19 20:45:32 -0300

    Componentes criados e rotas ajustadas

--- projeto-tradutor/src/app/app.routes.ts
+++ projeto-tradutor/src/app/app.routes.ts
@@ -1,12 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TelaLoginComponent } from './tela-login/tela-login.component';
+import { TelaCadastroComponent } from './tela-cadastro/tela-cadastro.component';
 
 export const routes: Routes = [
     {
     path:'',
-    redirectTo: 'HomeComponent'},
-    
+    component: HomeComponent
+    },
+        
     {      
     path: 'login',
-    redirectTo: 'TelaLoginComponent'
+    component: TelaLoginComponent
+    },
+
+    {
+    path:'cadastro',
+    component: TelaCadastroComponent
+
     }
 ];
