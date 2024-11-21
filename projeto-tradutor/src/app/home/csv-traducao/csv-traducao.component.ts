5775f1a96a8293dd11b7468469ff2e8500cc6a34
islanpedro01 <islanpedro.12@hotmail.com>
2024-11-20 22:39:54 -0300

    Add new components

---
+++ projeto-tradutor/src/app/home/csv-traducao/csv-traducao.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-csv-traducao',
+  imports: [],
+  templateUrl: './csv-traducao.component.html',
+  styleUrl: './csv-traducao.component.css'
+})
+export class CsvTraducaoComponent {
+
+}
---
+++ projeto-tradutor/src/app/home/url-traducao/url-traducao.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-url-traducao',
+  imports: [],
+  templateUrl: './url-traducao.component.html',
+  styleUrl: './url-traducao.component.css'
+})
+export class UrlTraducaoComponent {
+
+}
