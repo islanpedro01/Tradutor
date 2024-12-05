b27955fd995f771bf90700a92a382c6699c32520
islanpedro01 <islan.figueiredo@academico.ifpb.edu.br>
2024-12-04 22:34:23 -0300

    Sobre Nós feito

--- projeto-tradutor/src/app/tela-login/tela-login.component.ts
+++ projeto-tradutor/src/app/tela-login/tela-login.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-tela-login',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './tela-login.component.html',
   styleUrl: './tela-login.component.css'
 })
